9ba2eac124b934a65c73c7ad078390c926acd8ef
AFHINORS <168036386+AFHINORS@users.noreply.github.com>
2024-04-25 11:31:33 +0330

    a simple project  for create chatbot
    using streamlit
    using gpt4free

---
+++ chatbot.py
@@ -0,0 +1,20 @@
+import g4f
+import streamlit as st
+
+st.title('Powered By Shayan Farahani')
+
+inp = st.text_area('هر سوالی دارید بپرسید')
+
+ok = st.button('Lets Go')
+
+if inp != "" and ok:
+    response =g4f.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=[{"role": "user", "content": inp}],
+        stream=True
+
+    )
+
+    for message in response:
+        m = "".join(message)
+        st.write(m)
